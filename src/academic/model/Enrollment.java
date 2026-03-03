d023a9f487f95160a1178e16444ea2eaae3a7f5c
KelvinMarpaung18 <kelvinmarpaung088@gmail.com>
2026-03-03 10:14:50 +0700

    Update Enrollment.java

--- src/academic/model/Enrollment.java
+++ src/academic/model/Enrollment.java
@@ -1,11 +1,50 @@
 package academic.model;
 
 /**
- * @author NIM Nama
- * @author NIM Nama
+ * @author 12S24018 KELVIN YOHANES PUTRA
+ 
  */
 public class Enrollment {
+    private String courseCode;
+    private String studentId;
+    private String academicYear;
+    private String semester;
+    private String grade; 
 
-    // class definition
+    public Enrollment(String courseCode, String studentId, String academicYear, String semester) {
+        this.courseCode = courseCode;
+        this.studentId = studentId;
+        this.academicYear = academicYear;
+        this.semester = semester;
+        this.grade = "None"; // Inisialisasi grade default "None"
+    }
 
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public String getAcademicYear() {
+        return academicYear;
+    }
+
+    public String getSemester() {
+        return semester;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    @Override
+    public String toString() {
+        return courseCode + "|" + studentId + "|" + academicYear + "|" + semester + "|" + grade;
+    }
 }
