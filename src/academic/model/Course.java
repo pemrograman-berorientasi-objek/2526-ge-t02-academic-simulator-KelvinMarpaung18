7efed32832aad49dc9be29a6546a3f87b2c2eeca
KelvinMarpaung18 <kelvinmarpaung088@gmail.com>
2026-03-03 10:14:33 +0700

    Update Course.java

--- src/academic/model/Course.java
+++ src/academic/model/Course.java
@@ -1,11 +1,42 @@
 package academic.model;
 
 /**
- * @author NIM Nama
- * @author NIM Nama
+ * @author 12S24018 KELVIN YOHANES PUTRA
+ 
  */
 public class Course {
+    private String code;
+    private String name;
+    private int credits;
+    private String grade;
 
-    // class definition
+    public Course(String code, String name, int credits, String grade) {
+        this.code = code;
+        this.name = name;
+        this.credits = credits;
+        this.grade = grade;
+    }
 
+    // Anda bisa menambahkan getters jika diperlukan untuk keperluan lain,
+    // namun untuk tugas ini, method toString() sudah cukup.
+    public String getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCredits() {
+        return credits;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    @Override
+    public String toString() {
+        return code + "|" + name + "|" + credits + "|" + grade;
+    }
 }
